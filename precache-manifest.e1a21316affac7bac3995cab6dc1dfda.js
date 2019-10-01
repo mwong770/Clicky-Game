2a5230b907e145b8528eb5185749f56775d4bd71
Maria Wong <maria@Marias-MBP.attlocal.net>
2019-09-30 23:37:55 -0400

    Updates

---
+++ precache-manifest.e1a21316affac7bac3995cab6dc1dfda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d15ec33196a1fee2a48929dff1b393",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "03a5a8bb4ef9a4b7bd04",
+    "url": "/Clicky-Game/static/css/main.d3b8cd41.chunk.css"
+  },
+  {
+    "revision": "754ea6da2aadcc49e8aa",
+    "url": "/Clicky-Game/static/js/2.20c44714.chunk.js"
+  },
+  {
+    "revision": "03a5a8bb4ef9a4b7bd04",
+    "url": "/Clicky-Game/static/js/main.6376945c.chunk.js"
+  },
+  {
+    "revision": "8e05f7470861997906db",
+    "url": "/Clicky-Game/static/js/runtime-main.04dadfb0.js"
+  }
+]);
